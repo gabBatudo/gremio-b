2d094ae25e33ba222761fcc0b6583e93fa8bdd52
GabPossela <80076413+GabPossela@users.noreply.github.com>
2021-09-21 15:47:37 -0300

    adicionado as postagens

--- index.php
+++ index.php
@@ -3,18 +3,33 @@
 
     <section class="bg-cor" id="secIndex">
         <div class="container bg-cor">
+
             <div class="row d-flex justify-content-center py-5">
                 <div class="col-auto">
-                    <!-- Cards com os textos. Caso quiser, se pode fazer um array para não encher demais esse arquivo !-->
                     <div class="card" style="width: 65rem;">
                     <div class="card-body d-flex flex-column justify-content-center">
-                        <!-- Conteúdo dos textos !-->
-                        <h5 class="card-title text-center">Paulo Freire</h5>
-                        <p class="card-text text-justify lados">Na semana dos dias 3 a 7 de maio, foi comemorada a Semana Paulo Freire na ETEC Jadyr Salles. Nesta semana, os alunos realizaram diversas atividades especiais para comemorar a semana do educador.</p>
-                        <p class="card-text text-justify lados">Estas atividades estarão expostas na aba "Galeria" para que todos possam ver e aprender um pouco sobre a vida de Paulo Freire. As duas atividades expostas são: Caricaturas do educador e redações respondendo algumas perguntas sobre a vida de Freire.</p>
+                        <h5 class="card-title text-center">Monitorias</h5>
+                        <p class="card-text text-justify lados">Olá</p>
+                        <p class="card-text text-justify lados">Nosso querido Professor Landão está realizando um mini curso às segundas-feiras de JAVASCRIPT das 14:30 as 15:30 Hs e às terças-feiras, PYTHON, das 13:30 as 14:30 Hs, contando também com um reforço geral às quartas-feiras de Manutenção, banco de dados e programação dependendo da dificuldade do aluno que vier, das 15:30 as 16:30 Hs.</p>
+                        <p class="card-text text-justify lados">O Grêmio agradece a cooperação de todos!</p>
                     </div>
                     <div class="card-footer">
-                        Escrito por <cite title="Escrito por">João Ferreira</cite> <!-- Citação de quem escreveu o texto!-->
+                        Escrito por <cite title="Escrito por">João Ferreira</cite>
+                    </div>
+                </div>
+            </div>
+
+            <div class="row d-flex justify-content-center py-5">
+                <div class="col-auto">
+                    <div class="card" style="width: 65rem;">
+                    <div class="card-body d-flex flex-column justify-content-center">
+                        <h5 class="card-title text-center">Monitorias</h5>
+                        <p class="card-text text-justify lados">Olá a todos!!</p>
+                        <p class="card-text text-justify lados">Você sabia do Projeto de Monitorias do Grêmio? Caso você esteja com dificuldade em Física, você já pode se inscrever ás aulas de monitoria clicando <a href="https://forms.gle/wqAv8cBAg4EDsfTPA">neste link</a> e comparecer todas as Quartas-feiras a partir do dia 01-09-2021 15:30 as 16:30 – Via TEAMS</p>
+                        <p class="card-text text-justify lados">O Grêmio agradece a cooperação de todos!</p>
+                    </div>
+                    <div class="card-footer">
+                        Escrito por <cite title="Escrito por">João Ferreira</cite>
                     </div>
                 </div>
             </div>
@@ -33,6 +48,23 @@
                     </div>
                 </div>
             </div>
+
+            <div class="row d-flex justify-content-center py-5">
+                <div class="col-auto">
+                    <!-- Cards com os textos. Caso quiser, se pode fazer um array para não encher demais esse arquivo !-->
+                    <div class="card" style="width: 65rem;">
+                    <div class="card-body d-flex flex-column justify-content-center">
+                        <!-- Conteúdo dos textos !-->
+                        <h5 class="card-title text-center">Paulo Freire</h5>
+                        <p class="card-text text-justify lados">Na semana dos dias 3 a 7 de maio, foi comemorada a Semana Paulo Freire na ETEC Jadyr Salles. Nesta semana, os alunos realizaram diversas atividades especiais para comemorar a semana do educador.</p>
+                        <p class="card-text text-justify lados">Estas atividades estarão expostas na aba "Galeria" para que todos possam ver e aprender um pouco sobre a vida de Paulo Freire. As duas atividades expostas são: Caricaturas do educador e redações respondendo algumas perguntas sobre a vida de Freire.</p>
+                    </div>
+                    <div class="card-footer">
+                        Escrito por <cite title="Escrito por">João Ferreira</cite> <!-- Citação de quem escreveu o texto!-->
+                    </div>
+                </div>
+            </div>
+
         </div>
     </section>
     
